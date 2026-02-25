122aacf5240e072e77eaf9a9fb2da6e428536e7a
elysia <3302843873@qq.com>
2026-02-25 18:11:56 +0800

    Changed font to maplemono

--- kernel/src/graphics.rs
+++ kernel/src/graphics.rs
@@ -1,6 +1,6 @@
 use bootloader_api::BootInfo;
 use spin::Mutex;
-use spleen_font::{FONT_6X12, FONT_12X24, FONT_16X32, FONT_32X64, PSF2Font};
+use spleen_font::{FONT_6X12, FONT_8X16, FONT_12X24, FONT_16X32, FONT_32X64, PSF2Font};
 
 use crate::graphics::{
     framebuffer::{Canvas, FRAME_BUFFER},
@@ -10,9 +10,11 @@ use crate::graphics::{
 pub mod framebuffer;
 pub mod tty;
 
+pub static FONT: &[u8] = include_bytes!("../../maplemono.psf");
+
 pub fn init(boot_info: &'static mut bootloader_api::info::FrameBuffer) {
     FRAME_BUFFER.get_or_init(|| Mutex::new(Canvas::new(boot_info)));
-    let tty = TTY.get_or_init(|| Mutex::new(Tty::new(PSF2Font::new(FONT_12X24).unwrap())));
+    let tty = TTY.get_or_init(|| Mutex::new(Tty::new(PSF2Font::new(FONT).unwrap())));
 
     tty.lock().draw_wallpaper();
 }
--- libc-test/main.c
+++ libc-test/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	//while(1) {
+	while(1) {
     printf("Hello world from Relibc printf LETSGOOOOOOOOO!!!!!!");
-	//}
+	}
     return 0;
 }
