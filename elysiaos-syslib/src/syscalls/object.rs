6cba01b899c1a6248eb5bf0d52d047c6db87e1b0
elysia <3302843873@qq.com>
2026-03-01 12:07:32 +0800

    Added C wrappers for object operations

--- elysiaos-syslib/src/syscalls/object.rs
+++ elysiaos-syslib/src/syscalls/object.rs
@@ -1,5 +1,6 @@
-use crate::{syscall, utils::SyscallResult};
+use crate::{syscall, utils::SyscallResult, wrap_c};
 
+wrap_c!(read_object(index: u64, buffer: &mut [u8]));
 pub fn read_object(index: u64, buffer: &mut [u8]) -> SyscallResult {
     syscall!(
         ReadObject,
@@ -9,6 +10,7 @@ pub fn read_object(index: u64, buffer: &mut [u8]) -> SyscallResult {
     )
 }
 
+wrap_c!(write_object(index: u64, buffer: &[u8]));
 pub fn write_object(index: u64, buffer: &[u8]) -> SyscallResult {
     syscall!(
         WriteObject,
@@ -18,6 +20,7 @@ pub fn write_object(index: u64, buffer: &[u8]) -> SyscallResult {
     )
 }
 
+wrap_c!(configurate_object(index: u64, request_num: u64, ptr: *mut u8));
 pub fn configurate_object(index: u64, request_num: u64, ptr: *mut u8) -> SyscallResult {
     syscall!(ConfigurateObject, index, request_num, ptr as u64)
 }
