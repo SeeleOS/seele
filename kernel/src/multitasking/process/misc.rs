1c6209e86babc4ef1e1e49a09d6ca2a9ca5ab393
elysia <3302843873@qq.com>
2026-03-04 15:48:23 +0800

    Moved init stack layout to misc.rs

--- kernel/src/multitasking/process/misc.rs
+++ kernel/src/multitasking/process/misc.rs
@@ -1,8 +1,12 @@
 use core::sync::atomic::AtomicU64;
 
 use alloc::{sync::Arc, vec::Vec};
+use elfloader::ElfBinary;
 
-use crate::object::{Object, tty_device::TtyDevice};
+use crate::{
+    misc::stack_builder::StackBuilder,
+    object::{Object, tty_device::TtyDevice},
+};
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct ProcessID(pub u64);
@@ -19,4 +23,27 @@ pub fn init_objects(objects: &mut Vec<Arc<dyn Object>>) {
     objects.push(Arc::new(TtyDevice)); // stdin (unimpllemented)
     objects.push(Arc::new(TtyDevice)); // stdout
     objects.push(Arc::new(TtyDevice)); // stderr
+}
+
+pub fn init_stack_layout(builder: &mut StackBuilder, file: &ElfBinary) {
+    // A. 先在栈的最顶端存入字符串 "init\0"
+    // 字符串占用 5 字节，为了对齐我们按 8 字节处理
+    let arg_str = builder.push_str("init");
+
+    // 手动移动指针存入字符串
+    //*virt_stack_write = (virt_stack_write).sub(16);
+    //core::ptr::copy_nonoverlapping(arg_str.as_ptr(), *virt_stack_write as *mut u8, str_len);
+
+    builder.push(0);
+    // B. 使用你的 write_and_sub 按照 ABI 逆序压栈
+    builder.push_aux_entries(file);
+
+    builder.push(0); // envp = 0
+
+    // argv
+    builder.push(0); // argv[1] == null (end)
+    builder.push(arg_str); // argv[0] ==  *arg_str
+
+    // argc (1 arguments)
+    builder.push(1);
 }
--- kernel/src/multitasking/process/process.rs
+++ kernel/src/multitasking/process/process.rs
@@ -1,4 +1,3 @@
-
 use alloc::{
     sync::{Arc, Weak},
     vec::Vec,
@@ -14,7 +13,7 @@ use crate::{
         memory::{allocate_kernel_stack, allocate_stack},
         process::{
             ProcessRef,
-            misc::{ProcessID, init_objects},
+            misc::{ProcessID, init_objects, init_stack_layout},
         },
         thread::{
             THREAD_MANAGER,
@@ -88,27 +87,4 @@ impl Process {
 
         process_arc.clone()
     }
-}
-
-fn init_stack_layout(builder: &mut StackBuilder, file: &ElfBinary) {
-    // A. 先在栈的最顶端存入字符串 "init\0"
-    // 字符串占用 5 字节，为了对齐我们按 8 字节处理
-    let arg_str = builder.push_str("init");
-
-    // 手动移动指针存入字符串
-    //*virt_stack_write = (virt_stack_write).sub(16);
-    //core::ptr::copy_nonoverlapping(arg_str.as_ptr(), *virt_stack_write as *mut u8, str_len);
-
-    builder.push(0);
-    // B. 使用你的 write_and_sub 按照 ABI 逆序压栈
-    builder.push_aux_entries(file);
-
-    builder.push(0); // envp = 0
-
-    // argv
-    builder.push(0); // argv[1] == null (end)
-    builder.push(arg_str); // argv[0] ==  *arg_str
-
-    // argc (1 arguments)
-    builder.push(1);
 }
