abada0155afb6fed03337d7f1881c617e8004d1c
elysia <3302843873@qq.com>
2026-03-05 20:50:22 +0800

    resets bold on reset_color()

--- kernel/src/graphics/tty/misc.rs
+++ kernel/src/graphics/tty/misc.rs
@@ -3,6 +3,7 @@ use crate::graphics::tty::{DEFAULT_FOREGROUND, EMPTY_BACKGROUND, Tty};
 impl<'a> Tty<'a> {
     pub fn reset_color(&mut self) {
         self.current_background = EMPTY_BACKGROUND;
-        self.current_foreground = DEFAULT_FOREGROUND
+        self.current_foreground = DEFAULT_FOREGROUND;
+        self.bold = false;
     }
 }
