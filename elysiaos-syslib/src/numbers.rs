28c799837a66f70f1eb05f5c8cbd9302565bf592
elysia <3302843873@qq.com>
2026-03-05 08:59:11 +0800

    Added get current directory on syscal wrapper

--- elysiaos-syslib/src/numbers.rs
+++ elysiaos-syslib/src/numbers.rs
@@ -13,4 +13,5 @@ pub enum SyscallNumber {
     WriteObject,
     ConfigurateObject,
     ChangeDirectory,
+    GetCurrentDirectory,
 }
--- elysiaos-syslib/src/syscalls/filesystem.rs
+++ elysiaos-syslib/src/syscalls/filesystem.rs
@@ -3,3 +3,7 @@ use crate::{syscall, utils::SyscallResult};
 pub fn change_dir(dir: *const i8, len: u64) -> SyscallResult {
     syscall!(ChangeDirectory, dir as u64, len)
 }
+
+pub fn get_current_directory(buf: &mut [u8]) -> SyscallResult {
+    syscall!(GetCurrentDirectory, buf.as_ptr() as u64, buf.len() as u64)
+}
