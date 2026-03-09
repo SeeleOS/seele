cd518dab49a3ce2f7aa4945abca65b988fe93a14
elysia <3302843873@qq.com>
2026-03-09 12:15:34 +0800

    reverted muttable refrence on writable and redable refs

--- kernel/src/object/mod.rs
+++ kernel/src/object/mod.rs
@@ -46,12 +46,12 @@ pub type ObjectResult<T> = Result<T, ObjectError>;
 
 pub trait Writable: Object {
     /// Write the content of [`buffer`] to [`self`]
-    fn write(&mut self, buffer: &[u8]) -> ObjectResult<usize>;
+    fn write(&self, buffer: &[u8]) -> ObjectResult<usize>;
 }
 
 pub trait Readable: Object {
     /// Reads the content of [`self`] and write them to [`buffer`]
-    fn read(&mut self, buffer: &mut [u8]) -> ObjectResult<usize>;
+    fn read(&self, buffer: &mut [u8]) -> ObjectResult<usize>;
 }
 
 pub fn get_object(id: u64) -> Option<Arc<dyn Object>> {
