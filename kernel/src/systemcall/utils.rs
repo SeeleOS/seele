f42afaa1b7250792243c1d4a0fc174feb0f0dda3
elysia <3302843873@qq.com>
2026-03-10 10:05:22 +0800

    Added define_syscall macro

--- kernel/src/systemcall/utils.rs
+++ kernel/src/systemcall/utils.rs
@@ -7,3 +7,40 @@ macro_rules! register_syscall {
         );
     };
 }
+
+#[macro_export]
+macro_rules! define_syscall {
+    ($name:ident, |$($arg_name:ident : $arg_type:ty),*| $body:block) => {
+        paste::paste! {
+        pub struct [<$name Impl>];
+
+        impl SyscallImpl for $name {
+            const ENTRY: SyscallNo = SyscallNo::$name;
+
+            fn handle_call(
+                arg1: u64, arg2: u64, arg3: u64,
+                arg4: u64, arg5: u64, arg6: u64,
+            ) -> Result<usize, SyscallError> {
+                let args = [arg1, arg2, arg3, arg4, arg5, arg6];
+                let mut idx = 0;
+
+                // Cast types
+                $(
+                    let $arg_name = match stringify!($arg_type) {
+                        "&str" => unsafe { from_cstr(args[idx] as *const u8)? },
+                        "i32"  => args[idx] as i32,
+                        "u32"  => args[idx] as u32,
+                        "usize" => args[idx] as usize,
+                        "*mut LinuxStat" => args[idx] as *mut LinuxStat,
+                        _ => args[idx] as $arg_type, // 默认强转
+                    };
+                    #[allow(unused_assignments)]
+                    { idx += 1; }
+                )*
+
+                $body
+            }
+        }
+        }
+    };
+}
