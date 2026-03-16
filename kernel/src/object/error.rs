3975009fecfd88554fd4412571bfef953b16e4b7
elysia <3302843873@qq.com>
2026-03-16 10:13:29 +0800

    added tryagain object error

--- kernel/src/object/error.rs
+++ kernel/src/object/error.rs
@@ -3,12 +3,14 @@ use crate::{misc::error::AsSyscallError, systemcall::error::SyscallError};
 #[derive(Debug)]
 pub enum ObjectError {
     DoesNotExist,
+    TryAgain,
     Other,
 }
 
 impl AsSyscallError for ObjectError {
     fn as_syscall_error(&self) -> crate::systemcall::error::SyscallError {
         match self {
+            Self::TryAgain => SyscallError::TryAgain,
             Self::DoesNotExist => SyscallError::BadFileDescriptor,
             Self::Other => SyscallError::other("object error other"),
         }
