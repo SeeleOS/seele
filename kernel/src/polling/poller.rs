8dec9a8816c7c7f14439da0564b3c7ff628fb359
elysia <3302843873@qq.com>
2026-03-19 15:25:56 +0800

    added new() to pollerobject

--- kernel/src/polling/poller.rs
+++ kernel/src/polling/poller.rs
@@ -3,4 +3,10 @@ use crate::object::Object;
 #[derive(Debug)]
 pub struct PollerObject {}
 
+impl PollerObject {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
 impl Object for PollerObject {}
