0eef27a87ba2c61297816373fe073e5f080dafc2
elysia <3302843873@qq.com>
2026-03-19 17:29:25 +0800

    added a converter from u64 to pollevent

--- kernel/src/polling/event.rs
+++ kernel/src/polling/event.rs
@@ -1,4 +1,14 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum Event {
     Keypress,
+    Other(u64),
+}
+
+impl From<u64> for Event {
+    fn from(value: u64) -> Self {
+        match value {
+            0 => Self::Keypress,
+            _ => Self::Other(value),
+        }
+    }
 }
