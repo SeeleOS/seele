4d59a2f58297656a5d467a7cb2b45c3dccad9bdb
elysia <3302843873@qq.com>
2026-03-19 16:19:15 +0800

    added styff

--- kernel/src/polling/poller.rs
+++ kernel/src/polling/poller.rs
@@ -1,11 +1,36 @@
-use crate::object::Object;
+use alloc::{sync::Arc, vec::Vec};
+
+use crate::{
+    object::{Object, misc::ObjectRef},
+    polling::event::Event,
+};
 
 #[derive(Debug)]
-pub struct PollerObject {}
+pub struct PollerObject {
+    entries: Vec<PollerEntry>,
+}
+
+#[derive(Debug)]
+struct PollerEntry {
+    event: Event,
+    object: ObjectRef,
+}
+
+impl PollerEntry {
+    fn new(object: ObjectRef, event: Event) -> Self {
+        Self { event, object }
+    }
+}
 
 impl PollerObject {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            entries: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, object: ObjectRef, event: Event) {
+        self.entries.push(PollerEntry::new(object, event));
     }
 }
 
