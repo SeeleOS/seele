9c7861866160e131a5df229e529d7600ca066e0f
elysia <3302843873@qq.com>
2026-03-19 18:38:17 +0800

    added proper Pollable Events

--- kernel/src/polling/event.rs
+++ kernel/src/polling/event.rs
@@ -1,13 +1,20 @@
+/// Events made by a specific object to a poller.
 #[derive(Debug, PartialEq, Eq)]
 pub enum PollableEvent {
-    Keypress,
+    CanBeRead,
+    CanBeWritten,
+    Error,
+    Closed,
     Other(u64),
 }
 
 impl From<u64> for PollableEvent {
     fn from(value: u64) -> Self {
         match value {
-            0 => Self::Keypress,
+            0 => Self::CanBeRead,
+            1 => Self::CanBeWritten,
+            2 => Self::Error,
+            3 => Self::Closed,
             _ => Self::Other(value),
         }
     }
