dde27929e30358deac08edecdaae5d12d5b65e19
elysia <3302843873@qq.com>
2026-03-19 13:53:35 +0800

    added new fn for control command

--- kernel/src/object/control.rs
+++ kernel/src/object/control.rs
@@ -1,4 +1,16 @@
+use crate::systemcall::error::SyscallError;
+
 pub enum Command {
     SetFlags,
     GetFlags,
+}
+
+impl Command {
+    pub fn new(val: u64) -> Result<Self, SyscallError> {
+        match val {
+            0 => Ok(Self::SetFlags),
+            1 => Ok(Self::GetFlags),
+            _ => Err(SyscallError::InvalidArguments),
+        }
+    }
 }
