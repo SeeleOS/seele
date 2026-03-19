3f06c70d45f74cf26a55b9e83573f81e5f6cdd47
elysia <3302843873@qq.com>
2026-03-19 14:58:24 +0800

    uses fn from linux on command

--- seele-syslib/src/syscalls/object.rs
+++ seele-syslib/src/syscalls/object.rs
@@ -7,18 +7,16 @@ pub enum Command {
     GetFlags = 1,
 }
 
-impl From<i32> for Command {
-    fn from(value: i32) -> Self {
-        Self::from(value as u64)
+impl Command {
+    pub fn from_linux(value: i32) -> Option<Self> {
+        Self::from_linux_u64(value as u64)
     }
-}
 
-impl From<u64> for Command {
-    fn from(value: u64) -> Self {
+    pub fn from_linux_u64(value: u64) -> Option<Self> {
         match value {
-            0 => Self::SetFlags,
-            1 => Self::GetFlags,
-            _ => panic!("invalid object control command: {value}"),
+            0 => Some(Self::SetFlags),
+            1 => Some(Self::GetFlags),
+            _ => None,
         }
     }
 }
@@ -45,12 +43,8 @@ pub fn configurate_object(object: u64, request_num: u64, ptr: *mut u8) -> Syscal
     syscall!(ConfigurateObject, object, request_num, ptr as u64)
 }
 
-pub fn control_object_raw(object: u64, command: u64, arg: u64) -> SyscallResult {
-    syscall!(ControlObject, object, command as u64, arg)
-}
-
 pub fn control_object(object: u64, command: Command, arg: u64) -> SyscallResult {
-    control_object_raw(object, command as u64, arg)
+    syscall!(ControlObject, object, command as u64, arg)
 }
 
 pub fn remove_object(object: u64) -> SyscallResult {
