7d9ebdc18716f40b764db89338197749be6d4cbf
elysia <3302843873@qq.com>
2026-03-20 10:37:45 +0800

    wow

--- seele-syslib/seele_sys.h
+++ seele-syslib/seele_sys.h
@@ -9,16 +9,4 @@
 #include "stdint.h"
 #include "stddef.h"
 
-constexpr static const uint32_t seele_ICANON = 2;
-
-constexpr static const uint32_t seele_ECHO = 8;
-
-constexpr static const uint32_t seele_ECHOE = 16;
-
-constexpr static const uint32_t seele_ECHOK = 32;
-
-constexpr static const uint32_t seele_ECHONL = 64;
-
-constexpr static const uintptr_t seele_NCCS = 32;
-
 #endif  // seele_syslib_h
