4b9c952deff133ea5a8b3b778b39e21614142aeb
elysia <3302843873@qq.com>
2026-03-20 22:06:38 +0800

    only locks the canvas once when drawing terminal

--- kernel/src/terminal/renderer.rs
+++ kernel/src/terminal/renderer.rs
@@ -1,20 +1,24 @@
 use os_terminal::DrawTarget;
-use spin::Mutex;
+use spin::{Mutex, MutexGuard};
 
 use crate::misc::framebuffer::Canvas;
 
 pub struct TermRenderer<'a> {
     pub(crate) canvas: &'a Mutex<Canvas>,
+    frame_canvas: Option<MutexGuard<'a, Canvas>>,
     pub width: u32,
     pub height: u32,
 }
 
 impl<'a> TermRenderer<'a> {
     pub fn new(canvas: &'a Mutex<Canvas>) -> Self {
-        let width = canvas.lock().width;
-        let height = canvas.lock().height;
+        let canvas_guard = canvas.lock();
+        let width = canvas_guard.width;
+        let height = canvas_guard.height;
+        drop(canvas_guard);
         Self {
             canvas,
+            frame_canvas: None,
             width,
             height,
         }
@@ -26,8 +30,22 @@ impl<'a> DrawTarget for TermRenderer<'a> {
         (self.width as usize, self.height as usize)
     }
 
+    fn begin_draw(&mut self) {
+        if self.frame_canvas.is_none() {
+            self.frame_canvas = Some(self.canvas.lock());
+        }
+    }
+
+    fn end_draw(&mut self) {
+        self.frame_canvas = None;
+    }
+
     #[inline(always)]
     fn draw_pixel(&mut self, x: usize, y: usize, rgb: os_terminal::Rgb) {
-        self.canvas.lock().write_pixel(x, y, (rgb.0, rgb.1, rgb.2));
+        let canvas = self
+            .frame_canvas
+            .as_mut()
+            .expect("begin_draw must be called before draw_pixel");
+        canvas.write_pixel(x, y, (rgb.0, rgb.1, rgb.2));
     }
 }
