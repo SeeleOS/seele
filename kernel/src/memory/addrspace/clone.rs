12642cbaf228081884144c672eb27608e6f511b4
elysia <3302843873@qq.com>
2026-03-24 15:35:42 +0800

    uses the old frame on addrspace lcone

--- kernel/src/memory/addrspace/clone.rs
+++ kernel/src/memory/addrspace/clone.rs
@@ -2,11 +2,15 @@ use core::ptr::copy_nonoverlapping;
 
 use x86_64::{
     VirtAddr,
-    structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB, Translate},
+    structures::paging::{
+        FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB, Translate, mapper::TranslateResult,
+    },
 };
 
 use crate::memory::{
-    page_table_wrapper::PageTableWrapped, paging::FRAME_ALLOCATOR, utils::apply_offset,
+    page_table_wrapper::PageTableWrapped,
+    paging::FRAME_ALLOCATOR,
+    utils::{apply_offset, as_cow_flags},
 };
 
 use super::AddrSpace;
@@ -21,29 +25,34 @@ impl AddrSpace {
         log::debug!("frame allocator locked");
 
         let mut new_page_table = PageTableWrapped::new_with_frame_allocator(&mut frame_allocator);
-        let old_page_table = &self.page_table;
+        let old_page_table = &mut self.page_table;
 
         for region in self.memory_areas.clone() {
-            let pages = Page::<Size4KiB>::range(
+            let pages = Page::<Size4KiB>::range_inclusive(
                 Page::containing_address(region.start),
                 Page::containing_address(region.end),
             );
 
+            // Loops through all of the mapped pages of the previous addr space
             for page in pages {
+                // Get the frame of the page on the old page table
                 if let Ok(frame) = old_page_table.inner.translate_page(page)
                     && page.start_address() < VirtAddr::new(KERNEL_MEM_START)
+                        // Get the flags
+                    && let TranslateResult::Mapped { flags, .. } =
+                        old_page_table.inner.translate(page.start_address())
+                    && flags.contains(PageTableFlags::WRITABLE)
                 {
                     unsafe {
+                        old_page_table
+                            .inner
+                            .update_flags(page, as_cow_flags(flags))
+                            .unwrap()
+                            .flush();
+                        // Maps the new page with the frame of the old page
                         new_page_table
                             .inner
-                            .map_to(
-                                page,
-                                frame,
-                                PageTableFlags::USER_ACCESSIBLE
-                                    | PageTableFlags::WRITABLE
-                                    | PageTableFlags::PRESENT,
-                                &mut *frame_allocator,
-                            )
+                            .map_to(page, frame, as_cow_flags(flags), &mut *frame_allocator)
                             .unwrap()
                             .flush()
                     };
