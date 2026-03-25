88fff1758c6de9d086003f47dbe258a42f35dbfe
elysia <3302843873@qq.com>
2026-03-25 11:31:55 +0800

    added default for signal action

--- kernel/src/signal/action.rs
+++ kernel/src/signal/action.rs
@@ -3,13 +3,16 @@ use alloc::vec::Vec;
 use crate::signal::{Signal, SignalHandlerFn};
 
 /// The action that a process will take when it got a signal
+#[derive(Default, Debug)]
 pub struct SignalAction {
     pub handler: SignalHandler,
     // Signals which the process will ignore when its in the signal handler
     pub ignored_signals: Vec<Signal>,
 }
 
+#[derive(Default, Debug)]
 pub enum SignalHandler {
+    #[default]
     Default,
     Ignore,
     Function(SignalHandlerFn),
