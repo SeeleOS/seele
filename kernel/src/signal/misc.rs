29a3db4bfe37530417867bc31aa1e80a45320acc
elysia <3302843873@qq.com>
2026-03-25 15:36:01 +0800

    added a get_signal_action

--- kernel/src/signal/misc.rs
+++ kernel/src/signal/misc.rs
@@ -1,7 +1,17 @@
 use alloc::vec::Vec;
+use seele_sys::signal::Signal;
 
-use crate::signal::{SIGNAL_AMOUNT, action::SignalAction};
+use crate::{
+    process::Process,
+    signal::{SIGNAL_AMOUNT, action::SignalAction},
+};
 
 pub fn default_signal_action_vec() -> Vec<SignalAction> {
     alloc::vec![SignalAction::default(); SIGNAL_AMOUNT]
+}
+
+impl Process {
+    pub fn get_signal_action(&mut self, signal: Signal) -> &mut SignalAction {
+        &mut self.signal_actions[signal as usize]
+    }
 }
