7869bc4fa5c15f65cc5d8af1a9dc7fdef832aebd
elysia <3302843873@qq.com>
2026-03-25 12:34:45 +0800

    nologner derives cloen and copy

--- kernel/src/signal/action.rs
+++ kernel/src/signal/action.rs
@@ -3,7 +3,7 @@ use alloc::vec::Vec;
 use crate::signal::{Signal, SignalHandlerFn};
 
 /// The action that a process will take when it got a signal
-#[derive(Default, Clone, Copy, Debug)]
+#[derive(Default, Debug)]
 pub struct SignalAction {
     pub handler: SignalHandler,
     // Signals which the process will ignore when its in the signal handler
