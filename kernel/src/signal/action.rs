61cd4218e1c5f346a7737b1f2e769a2c4f1b7ad4
elysia <3302843873@qq.com>
2026-03-25 12:36:03 +0800

    derives cloen (wtf)

--- kernel/src/signal/action.rs
+++ kernel/src/signal/action.rs
@@ -3,14 +3,14 @@ use alloc::vec::Vec;
 use crate::signal::{Signal, SignalHandlerFn};
 
 /// The action that a process will take when it got a signal
-#[derive(Default, Debug)]
+#[derive(Default, Clone, Debug)]
 pub struct SignalAction {
     pub handler: SignalHandler,
     // Signals which the process will ignore when its in the signal handler
     pub ignored_signals: Vec<Signal>,
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Clone, Debug)]
 pub enum SignalHandler {
     #[default]
     Default,
