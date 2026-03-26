19dbc0cc33a7ed25266ddaa0ea06509c5e983b4c
elysia <3302843873@qq.com>
2026-03-26 08:56:08 +0800

    added zlib to flake.nix

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
               rust-script
               cmake
               lld
+              zlib
               rustc
               rustfmt
               gmp
@@ -53,7 +54,7 @@
                                                export SYSROOT_DIR="/home/elysia/coding-project/seeleos/sysroot"
                             		      export TOOLCHAIN_DIR="/home/elysia/coding-project/seeleos/toolchain"
                                                   export REPO_ROOT="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
-                                                  export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+                                                  export LD_LIBRARY_PATH=${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
                                                   export PATH="$REPO_ROOT/.llvm/bin:$HOME/.cargo/bin:$TOOLCHAIN_DIR/misc/toolchain/bin:$PATH"
                                                   echo "[devshell] Ensuring Rust toolchain 'seele'..."
                                                   #(cd /home/elysia/coding-project/elysia-os/toolchain && ./install.rs) || echo "[devshell] install.rs failed"
