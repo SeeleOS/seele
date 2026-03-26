a98167b77a077552e65ce323f4a896340aabca59
elysia <3302843873@qq.com>
2026-03-26 21:14:43 +0800

    added memoryData type

--- kernel/src/memory/addrspace/mem_area.rs
+++ kernel/src/memory/addrspace/mem_area.rs
@@ -6,7 +6,7 @@ use x86_64::{
     },
 };
 
-use crate::memory::addrspace::KERNEL_MEM_START;
+use crate::{filesystem::path::Path, memory::addrspace::KERNEL_MEM_START};
 
 #[derive(Clone, Debug)]
 pub struct MemoryArea {
@@ -22,6 +22,7 @@ pub struct MemoryArea {
 pub enum Data {
     // Normal data that a process/thread can write to. Aka anonymus.
     Normal,
+    File { offset: u64, path: Path },
 }
 
 impl MemoryArea {
