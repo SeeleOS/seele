cc46f78c3ca54caf445196a30d7f1cf1c00b50d6
elysia <3302843873@qq.com>
2026-03-28 15:46:51 +0800

    added get_processes_in_group fn

--- kernel/src/process/group.rs
+++ kernel/src/process/group.rs
@@ -1,5 +1,10 @@
 use core::sync::atomic::AtomicU64;
 
+use alloc::vec::Vec;
+use seele_sys::signal::Signal;
+
+use crate::process::{ProcessRef, manager::Manager};
+
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct ProcessGroupID(pub u64);
 
@@ -8,5 +13,19 @@ impl Default for ProcessGroupID {
         static NEXT_GID: AtomicU64 = AtomicU64::new(0);
 
         Self(NEXT_GID.fetch_add(1, core::sync::atomic::Ordering::Relaxed))
+    }
+}
+
+impl Manager {
+    pub fn get_processes_in_group(&mut self, group_id: ProcessGroupID) -> Vec<ProcessRef> {
+        let mut processes = Vec::new();
+
+        for (_, process) in &mut self.processes {
+            if process.lock().group_id == group_id {
+                processes.push(process.clone());
+            }
+        }
+
+        processes
     }
 }
