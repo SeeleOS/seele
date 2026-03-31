bb767f736332cb360f863064fe1c3d3def9a8261
elysia <3302843873@qq.com>
2026-03-31 10:08:06 +0800

    added a device system for object

---
+++ kernel/src/object/device.rs
@@ -0,0 +1,21 @@
+use alloc::{collections::btree_map::BTreeMap, string::String, vec::Vec};
+use seele_sys::{SyscallResult, errors::SyscallError};
+
+use crate::object::misc::ObjectRef;
+
+lazy_static::lazy_static! {
+    pub static ref DEVICES: BTreeMap<String,ObjectRef> = {
+        let devices = BTreeMap::new();
+
+        devices
+    };
+}
+
+pub fn get_device(name: String) -> SyscallResult<ObjectRef> {
+    DEVICES
+        .iter()
+        .find(|(name, _)| name == name)
+        .ok_or(SyscallError::InvalidArguments)
+        .map(|(_, obj)| obj)
+        .cloned()
+}
--- kernel/src/object/mod.rs
+++ kernel/src/object/mod.rs
@@ -14,6 +14,7 @@ use crate::{
 
 pub mod config;
 pub mod control;
+pub mod device;
 pub mod error;
 pub mod misc;
 pub mod traits;
