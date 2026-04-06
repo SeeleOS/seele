b1600bbee4eb2765a95c4cf646bd5fbaed824bf6
elysia <3302843873@qq.com>
2026-04-06 21:45:40 +0800

    registered LinkFile syscall

--- kernel/src/systemcall/table.rs
+++ kernel/src/systemcall/table.rs
@@ -63,7 +63,8 @@ pub static SYSCALL_TABLE: [Option<SyscallHandler>; 1500] = {
         Sleep,
         CreateDirectory,
         SeekObject,
-        ThreadClone
+        ThreadClone,
+        LinkFile
     );
 
     table
