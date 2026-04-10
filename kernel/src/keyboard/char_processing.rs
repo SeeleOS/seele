9ab9140f5f2400260baaed35883d1981eb3be08b
elysia <3302843873@qq.com>
2026-04-10 15:08:15 +0800

    finished ctrl + c processing

--- kernel/src/keyboard/char_processing.rs
+++ kernel/src/keyboard/char_processing.rs
@@ -1,14 +1,14 @@
 use core::char;
 
 use alloc::collections::vec_deque::VecDeque;
-use seele_sys::signal::Signal;
+use bootloader_api::info;
+use seele_sys::{abi::object::TerminalInfo, signal::Signal};
 use spin::mutex::Mutex;
 
 use crate::{
     keyboard::decoding_task::KEYBOARD_QUEUE,
     object::tty_device::{get_default_tty, wake_tty_poller_readable},
     print,
-    process::misc::with_current_process,
     terminal::{
         misc::{LINE_BUFFER, flush_line_buffer},
         state::DEFAULT_TERMINAL,
@@ -16,8 +16,27 @@ use crate::{
     thread::THREAD_MANAGER,
 };
 
+fn handle_interrupt_char(info: &TerminalInfo) {
+    if let Some(group_id) = *get_default_tty().active_group.lock()
+        && info.send_sig_on_special_chars
+    {
+        LINE_BUFFER.lock().clear();
+        group_id
+            .get_processes()
+            .iter()
+            .for_each(|process| process.lock().send_signal(Signal::Interrupt));
+        THREAD_MANAGER.get().unwrap().lock().wake_keyboard();
+        wake_tty_poller_readable();
+    }
+}
+
 pub fn process_char(char: char) {
     let info = *DEFAULT_TERMINAL.get().unwrap().lock().info.lock();
+
+    if char == '\x03' {
+        handle_interrupt_char(&info);
+        return;
+    }
 
     if !info.canonical {
         // In noncanonical mode, userspace handles line editing and submission.
@@ -54,16 +73,7 @@ pub fn process_char(char: char) {
                 }
             }
         }
-        '\x03' => {
-            if let Some(group_id) = *get_default_tty().active_group.lock() {
-                LINE_BUFFER.lock().clear();
-
-                group_id
-                    .get_processes()
-                    .iter()
-                    .for_each(|f| f.lock().send_signal(Signal::Interrupt));
-            }
-        }
+        '\x03' => handle_interrupt_char(&info),
         _ => {
             if info.echo {
                 print!("{char}");
