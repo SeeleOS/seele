f0fe8624257fc2aec4402627269d46ce14c58992
elysia <3302843873@qq.com>
2026-04-11 17:39:31 +0800

    added master and slave refs to PtyShared

--- kernel/src/terminal/pty/mod.rs
+++ kernel/src/terminal/pty/mod.rs
@@ -1,8 +1,8 @@
-use acpi::sdt::fadt::ArmBootArchFlags;
 use alloc::sync::Arc;
 use spin::Mutex;
 
 use crate::{
+    object::misc::ObjectRef,
     process::misc::with_current_process,
     terminal::pty::{master::PtyMaster, shared::PtyShared, slave::PtySlave},
 };
@@ -14,13 +14,21 @@ pub mod slave;
 pub fn create_pty() -> (i32, i32) {
     let shared = Arc::new(Mutex::new(PtyShared::default()));
 
-    let master = PtyMaster::new(shared.clone());
-    let slave = PtySlave::new(shared.clone());
+    let master = Arc::new(PtyMaster::new(shared.clone()));
+    let slave = Arc::new(PtySlave::new(shared.clone()));
+    let master_object: ObjectRef = master.clone();
+    let slave_object: ObjectRef = slave.clone();
+
+    {
+        let mut shared = shared.lock();
+        shared.master = Some(Arc::downgrade(&master_object));
+        shared.slave = Some(Arc::downgrade(&slave_object));
+    }
 
     with_current_process(|process| {
         (
-            process.push_object(Arc::new(master)) as i32,
-            process.push_object(Arc::new(slave)) as i32,
+            process.push_object(master_object) as i32,
+            process.push_object(slave_object) as i32,
         )
     })
 }
--- kernel/src/terminal/pty/shared.rs
+++ kernel/src/terminal/pty/shared.rs
@@ -1,7 +1,11 @@
-use alloc::collections::vec_deque::VecDeque;
+use alloc::{collections::vec_deque::VecDeque, sync::Weak};
+
+use crate::object::Object;
 
 #[derive(Debug, Default)]
 pub struct PtyShared {
     pub from_master: VecDeque<u8>,
     pub from_slave: VecDeque<u8>,
+    pub master: Option<Weak<dyn Object>>,
+    pub slave: Option<Weak<dyn Object>>,
 }
