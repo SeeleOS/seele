cb72990c25e93837deba35bf7a6c1359092e55fd
elysia <3302843873@qq.com>
2026-04-12 12:47:45 +0800

    snapshot: automatically calculates the offset instead of hardcoding

--- kernel/src/misc/snapshot.rs
+++ kernel/src/misc/snapshot.rs
@@ -1,6 +1,7 @@
 // Snapshot of the operating system. Including registers.
 // Also known as Frame, Context, etc.
 
+use core::mem::offset_of;
 use x86_64::structures::idt::InterruptStackFrame;
 
 #[repr(C)]
@@ -53,40 +54,59 @@ impl Snapshot {
         let mut snp = Self::default();
         unsafe {
             core::arch::asm!(
-                // 1. 保存通用寄存器到结构体偏移
-                "mov [rdi + 0x00], r15",
-                "mov [rdi + 0x08], r14",
-                "mov [rdi + 0x10], r13",
-                "mov [rdi + 0x18], r12",
-                "mov [rdi + 0x20], r11",
-                "mov [rdi + 0x28], r10",
-                "mov [rdi + 0x30], r9",
-                "mov [rdi + 0x38], r8",
-                "mov [rdi + 0x40], rdi", // 这里存的是原始的 rdi
-                "mov [rdi + 0x48], rsi",
-                "mov [rdi + 0x50], rbp",
-                "mov [rdi + 0x58], rbx",
-                "mov [rdi + 0x60], rdx",
-                "mov [rdi + 0x68], rcx",
-                "mov [rdi + 0x70], rax",
+                // Executor snapshots only need precise callee-saved registers and
+                // control state. Use rax as the output base so the live rdi value
+                // is captured instead of being overwritten by the snapshot pointer.
+                "mov [rax + {R15_OFF}], r15",
+                "mov [rax + {R14_OFF}], r14",
+                "mov [rax + {R13_OFF}], r13",
+                "mov [rax + {R12_OFF}], r12",
+                "mov [rax + {R11_OFF}], r11",
+                "mov [rax + {R10_OFF}], r10",
+                "mov [rax + {R9_OFF}], r9",
+                "mov [rax + {R8_OFF}], r8",
+                "mov [rax + {RDI_OFF}], rdi",
+                "mov [rax + {RSI_OFF}], rsi",
+                "mov [rax + {RBP_OFF}], rbp",
+                "mov [rax + {RBX_OFF}], rbx",
+                "mov [rax + {RDX_OFF}], rdx",
+                "mov [rax + {RCX_OFF}], rcx",
 
-                // 2. 特殊寄存器处理
-                "lea rax, [rip + 2f]",    // 捕获“跳出汇编”后的 RIP
-                "mov [rdi + 0x78], rax",   // rip 偏移是 0x80 (16*8)
+                "lea rdx, [rip + 2f]",
+                "mov [rax + {RIP_OFF}], rdx",
 
-                "mov rax, cs",
-                "mov [rdi + 0x80], rax",   // cs
+                "mov rdx, cs",
+                "mov [rax + {CS_OFF}], rdx",
 
-                "pushfq",                 // 捕获 RFLAGS
-                "pop qword ptr [rdi + 0x88]",
+                "pushfq",
+                "pop qword ptr [rax + {RFLAGS_OFF}]",
 
-                "mov [rdi + 0x90], rsp",   // rsp
+                "mov [rax + {RSP_OFF}], rsp",
 
-                "mov rax, ss",
-                "mov [rdi + 0x98], rax",   // ss
+                "mov rdx, ss",
+                "mov [rax + {SS_OFF}], rdx",
                 "2:",
-                in("rdi") &mut snp,        // 将结构体地址传入 rdi
-                options(nostack)
+                in("rax") &mut snp,
+                lateout("rdx") _,
+                R15_OFF = const offset_of!(Snapshot, r15),
+                R14_OFF = const offset_of!(Snapshot, r14),
+                R13_OFF = const offset_of!(Snapshot, r13),
+                R12_OFF = const offset_of!(Snapshot, r12),
+                R11_OFF = const offset_of!(Snapshot, r11),
+                R10_OFF = const offset_of!(Snapshot, r10),
+                R9_OFF = const offset_of!(Snapshot, r9),
+                R8_OFF = const offset_of!(Snapshot, r8),
+                RDI_OFF = const offset_of!(Snapshot, rdi),
+                RSI_OFF = const offset_of!(Snapshot, rsi),
+                RBP_OFF = const offset_of!(Snapshot, rbp),
+                RBX_OFF = const offset_of!(Snapshot, rbx),
+                RDX_OFF = const offset_of!(Snapshot, rdx),
+                RCX_OFF = const offset_of!(Snapshot, rcx),
+                RIP_OFF = const offset_of!(Snapshot, rip),
+                CS_OFF = const offset_of!(Snapshot, cs),
+                RFLAGS_OFF = const offset_of!(Snapshot, rflags),
+                RSP_OFF = const offset_of!(Snapshot, rsp),
+                SS_OFF = const offset_of!(Snapshot, ss),
             );
         }
         snp
