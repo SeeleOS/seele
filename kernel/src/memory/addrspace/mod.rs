710ee396078a7a8dd75de3652ff2523ae0ccb1a5
elysia <3302843873@qq.com>
2026-04-12 12:47:19 +0800

    enabled lazymapping

--- kernel/src/memory/addrspace/mod.rs
+++ kernel/src/memory/addrspace/mod.rs
@@ -29,7 +29,7 @@ pub mod mapping;
 pub mod mem_area;
 pub mod misc;
 
-pub const LAZY_MAP: bool = false;
+pub const LAZY_MAP: bool = true;
 
 const USER_MEM_START: u64 = 0x30_0000_0000;
 pub const KERNEL_MEM_START: u64 = 0xFFFF_8000_1000_0000;
