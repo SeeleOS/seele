92b3815360b59a6ca3c42820b57b3ba3937068d8
elysia <3302843873@qq.com>
2026-04-18 09:33:14 +0800

    initscript: fix

--- misc/init.sh
+++ misc/init.sh
@@ -4,7 +4,20 @@ export PATH=/bin:/usr/bin
 export TERM=xterm-256color
 export HOME=/root
 
-/bin/xinit
+mkdir -p /run/dbus
+if [ -x /usr/bin/dbus-daemon ] && [ ! -S /run/dbus/system_bus_socket ]; then
+    if [ -f /usr/share/dbus-1/system.conf ] || [ -f /etc/dbus-1/system.conf ]; then
+        /usr/bin/dbus-daemon --system --fork --nopidfile
+    else
+        /usr/bin/dbus-daemon \
+            --session \
+            --fork \
+            --nopidfile \
+            --address=unix:path=/run/dbus/system_bus_socket
+    fi
+fi
+
+/bin/xinit /etc/X11/xinit/xinitrc -- /usr/bin/X :0 -dumbSched -extension GLX -extension DRI2 -extension DRI3
 status=$?
 
 echo "init: xinit exited with status ${status}"
