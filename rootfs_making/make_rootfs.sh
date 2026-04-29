aab7b838534c0f0c7b8110944e9adbf39ec33616
elysia <3302843873@qq.com>
2026-04-29 15:43:04 +0800

    clean sddm greeter config

--- rootfs_making/make_rootfs.sh
+++ rootfs_making/make_rootfs.sh
@@ -213,6 +213,9 @@ arch_chroot passwd -d root
 arch_chroot systemctl enable seatd.service
 arch_chroot systemctl enable sddm.service
 
+sudo rm -rf "${SYSROOT_DIR}/etc/systemd/system/sddm.service.d"
+sudo rm -f "${SYSROOT_DIR}/usr/local/bin/sddm-wayland-compositor.sh"
+
 install_sysroot_file "${ROOTFS_MAKING_DIR}/weston.ini" "${SYSROOT_DIR}/etc/xdg/weston/weston.ini"
 install_sysroot_file "${ROOTFS_MAKING_DIR}/xinitrc" "${SYSROOT_DIR}/etc/X11/xinit/xinitrc"
 install_sysroot_file "${ROOTFS_MAKING_DIR}/20-modesetting.conf" "${SYSROOT_DIR}/etc/X11/xorg.conf.d/20-modesetting.conf"
