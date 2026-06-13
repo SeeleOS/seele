4f5205611a5cf0e6f7a3f2b81af82d519b6e1916
elysia <3302843873@qq.com>
2026-06-13 16:12:36 +0800

    align kernel heap base

--- kernel/src/memory/heap.rs
+++ kernel/src/memory/heap.rs
@@ -18,7 +18,7 @@ static HEAP_ALLOCATOR: LockedHeapWithRescue<32> = LockedHeapWithRescue::new(grow
 static HEAP_MAPPED_BYTES: AtomicUsize = AtomicUsize::new(0);
 
 // Memory area for the heap
-pub const HEAP_START: usize = 0xFFFF_FFFF_4444_0000;
+pub const HEAP_START: usize = 0xFFFF_FFFF_4000_0000;
 pub const HEAP_SIZE: usize = 1024 * 1024 * 1024;
 pub const INITIAL_HEAP_SIZE: usize = 64 * 1024 * 1024;
 const HEAP_GROWTH_SIZE: usize = 8 * 1024 * 1024;
