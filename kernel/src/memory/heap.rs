7493aff4bf5768992bf122a7132c4fe32d8b311b
elysia <3302843873@qq.com>
2026-06-13 15:58:34 +0800

    expand kernel heap for plasma

--- kernel/src/memory/heap.rs
+++ kernel/src/memory/heap.rs
@@ -19,10 +19,10 @@ static HEAP_MAPPED_BYTES: AtomicUsize = AtomicUsize::new(0);
 
 // Memory area for the heap
 pub const HEAP_START: usize = 0xFFFF_FFFF_4444_0000;
-pub const HEAP_SIZE: usize = 256 * 1024 * 1024;
+pub const HEAP_SIZE: usize = 1024 * 1024 * 1024;
 pub const INITIAL_HEAP_SIZE: usize = 64 * 1024 * 1024;
 const HEAP_GROWTH_SIZE: usize = 8 * 1024 * 1024;
-const HEAP_PAGE_TABLE_RESERVE_SIZE: usize = 2 * 1024 * 1024;
+const HEAP_PAGE_TABLE_RESERVE_SIZE: usize = 4 * 1024 * 1024;
 pub const HEAP_BACKING_RESERVE_SIZE: usize = HEAP_SIZE + HEAP_PAGE_TABLE_RESERVE_SIZE;
 
 // Map the memory area for the heap from physical memory to virt memory
